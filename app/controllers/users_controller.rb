da559c1bf3dcd1df9a44ce47dba839ec6133ab03
EC2 Default User <ec2-user@ip-172-31-86-75.ec2.internal>
2022-05-09 02:18:09 +0000

    second Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,17 @@ class UsersController < ApplicationController
     end  
   end
   
+  def follows
+    user = User.find(params[:id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:id])
+    @users = user.followers
+  
+  end
+  
   
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,44 +1,20 @@
 Rails.application.routes.draw do
   
-  # get 'users/index'
-  # get 'users/show'
-  # get 'users/edit'
-  
-  
-  
   root to: "homes#top"
-  
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
   get 'home/about' => 'homes#about' , as: 'about'
   
-  get 'books' => 'books#index'
-  get 'users' => 'users#index'
-  
-  
-  # get 'books/new'
-  # get 'books/index'
-  # get 'books/show'
-  # get 'books/edit'
-  
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  
   resources :books do
-    
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
-    
   end
   
   
   
-  resources :users
-  
-  
-  # get 'users/:id' => 'users#show', as: 'user'
-  # get 'users/:id/edit' => 'users#edit', as: 'edit_user'
-  # patch 'users/:id' => 'users#update', as: 'update_user'
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member # 追加
+    get :followers, on: :member # 追加
+  end
   
 end
